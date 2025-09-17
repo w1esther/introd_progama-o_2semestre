61dbda2c673a5863ca47e941bb300d60701d3983
w1esther <w1mariaesther@gmail.com>
2025-09-16 21:38:17 -0300

    resolução de exercícios

---
+++ bloco1/ex1.py
@@ -0,0 +1,21 @@
+# @cikey 834c2e6fa9c10fdc8929a92de4d69a3d
+# @sid 20251174010006
+# @aid 0
+
+
+#begin_inputs
+
+# Defina as variáveis de entrada aqui
+# Não apague as linhas com #begin_inputs e #end_inputs
+
+num1 = float(input())
+num2 = float(input())
+
+soma = num1 + num2
+
+print(soma)
+
+#end_inputs
+
+# Faça o processamento e a exibição da saída a partir daqui
+	
