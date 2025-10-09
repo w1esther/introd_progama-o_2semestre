58356acf1961b3e11c1077896c8e70d4f97de4cd
Maria Esther <w1mariaesther@gmail.com>
2025-10-09 17:15:15 -0300

    Add files via upload

---
+++ jogo.py
@@ -0,0 +1,14 @@
+from random import randint
+n = int(input('tente adivinhar um número de 1 a 100: '))
+
+numero_secreto = randint(1, 100)
+
+while n != numero_secreto:
+    if n < numero_secreto:
+        print('o valor secreto é maior! Tente novamente')
+        n = int(input('tente adivinhar um número de 1 a 100: '))
+    elif n > numero_secreto:
+        print('o valor secreto é menor! Tente novamente')
+        n = int(input('tente adivinhar um número de 1 a 100: '))
+
+print(f'Parabéns, você acertou! O número secreto é: {numero_secreto}')
