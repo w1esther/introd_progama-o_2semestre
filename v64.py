e0ee989f46ecc5c758814f75d7229528bd77f408
Maria Esther <w1mariaesther@gmail.com>
2025-10-11 08:33:13 -0300

    adição de exercício Craps corrigido

--- v64.py
+++ v64.py
@@ -15,11 +15,13 @@ elif valor_dado == 2 or valor_dado == 3 or valor_dado == 12:
     print('Voce perdeu!')
 else:
     ponto = valor_dado
-    valor_dado = int(input())
-    while valor_dado != ponto:
-        valor_dado = int(input())
-        if valor_dado == 7:
-            valor_dado = int(input())
-            if valor_dado == ponto:
-                print('Voce perdeu!')
-    print('Voce ganhou!')  
+    valor_dado = int(input('valor sorteado no dado: '))
+    while True:
+        if valor_dado == ponto:
+            print('Voce ganhou!')
+            break
+        elif valor_dado == 7:
+            print('Voce perdeu!')
+            break
+        else:
+            valor_dado = int(input('valor sorteado no dado: '))
